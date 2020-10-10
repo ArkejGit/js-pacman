3f6a9068bb2242cea66e9b4acb2279829763f547
Robert Krawczyk <robert@roberts-macbook-pro.home>
2020-10-10 17:10:40 +0200

    index.js scaffolding & setup

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Working!');
+import { LEVEL, OBJECT_TYPE } from './setup';
+
+//  DOM elements
+const gameGrid = document.querySelector('#game');
+const scoreTable = document.querySelector('#score');
+const startButton = document.querySelector('#start-button');
+
+//  Game constants
+const POWER_PILL_TIME = 10000;
+const GLOBAL_SPEED = 80;
+
+// Initial setup
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+const gameOver = (pacman, grid) => {
+
+}
+
+const checkCollision = (pacman, ghosts) => {
+
+}
+
+const gameLoop = (pacman, ghosts) => {
+
+}
+
+const startGame = () => {
+
+}
